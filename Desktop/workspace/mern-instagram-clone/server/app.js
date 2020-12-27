6cc499b32d07b95229270647cb506ae477d7c50c
djdaantruong <daantd.a12017@gmail.com>
2020-12-27 23:45:05 +0700

    learn middleware & connect mongo alas

--- Desktop/workspace/mern-instagram-clone/server/app.js
+++ Desktop/workspace/mern-instagram-clone/server/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const { MONGOURL } = require('./keys')
+
 const app = express()
 const PORT = 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello Instagram')
+mongoose.connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo yeah!')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('error connecting', err)
+})
+
 
 app.listen(PORT, () => {
     console.log('Server is running on', PORT)
---
+++ Desktop/workspace/mern-instagram-clone/server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURL: 'mongodb+srv://Djdaan:abc1234@react-blog.g9qch.mongodb.net/instagramclone?retryWrites=true&w=majority'
+}
