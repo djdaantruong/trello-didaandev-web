426ddab72a3e4d614f88016b12d9218500a9d909
apple <apple@apples-MacBook-Pro.local>
2021-08-18 02:02:55 +0700

    Design trello ui.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,117 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello">Hello</h1>
+    <div className="trelo-didaandev-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1536x1137.jpg" alt="trungquan-alt-img" />
+              Tille: Trungquan
+            </li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+            <li>work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
